f054c0c9c26ac12d3c85fd4adfd73ad8ca6bab1f
Armando Ruocco <armando.ruocco@enterprisedb.com>
2023-05-21 13:31:11 +0200

    chore: never delete pods of unscheduled pods
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- internal/controller/cluster_controller.go
+++ internal/controller/cluster_controller.go
@@ -692,6 +692,11 @@ func (r *ClusterReconciler) deleteEvictedOrUnscheduledInstances(
 			"Deleted evicted/unscheduled Pod %v",
 			instance.Name)
 
+		// we never delete the PVCs of unscheduled pods
+		if utils.IsPodUnscheduled(instance) {
+			continue
+		}
+
 		// we never delete the pvc unless we are in node Maintenance Window and the Reuse PVC is false
 		if !cluster.IsNodeMaintenanceWindowInProgress() || cluster.IsReusePVCEnabled() {
 			continue
@@ -707,7 +712,7 @@ func (r *ClusterReconciler) deleteEvictedOrUnscheduledInstances(
 			return nil, err
 		}
 		r.Recorder.Eventf(cluster, "Normal", "DeletePVCs",
-			"Deleted evicted/unscheduled Pod %v PVCs",
+			"Deleted evicted Pod %v PVCs",
 			instance.Name)
 	}
 
